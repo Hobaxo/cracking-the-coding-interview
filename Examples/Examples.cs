d5187b8f2b3b854f2526d871ef210a6208d16814
Hobaxo <herberthfub@gmail.com>
2023-01-31 23:34:40 -0600

    Creation of Examples project

---
+++ Examples/Examples.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+
+namespace Examples
+{
+    internal class Examples
+    {
+        private static void Main()
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+        }
+    }
+}
