2df0c5d14c147089ad8330067318a03189eea7bf
Hobaxo <herberthfub@gmail.com>
2023-02-07 16:36:26 -0600

    Example #5: O(ab)

--- Examples/BigO/BigO.cs
+++ Examples/BigO/BigO.cs
@@ -86,12 +86,12 @@ namespace Examples.BigO
             Console.WriteLine($"Elapsed time:{elapsedTime}-Elapsed Ticks:{stopWatch.ElapsedTicks}\n");
         }
 
-        // O(ab) - two different arrays
+        // O(ab) - two different inputs (arrays)
         public static void PrintUnorderedPairs(int[] arrayA, int[] arrayB)
         {
             Stopwatch firstForStopWatch = new Stopwatch();
             Stopwatch secondForStopWatch = new Stopwatch();
-            
+
             firstForStopWatch.Start();
             for (int i = 0; i < arrayA.Length; i++)
             {
@@ -103,14 +103,48 @@ namespace Examples.BigO
                         Console.Write($"i:{arrayA[i]}, j:{arrayB[j]} | ");
                     }
                 }
-                Console.Write('\n');
+
                 secondForStopWatch.Stop();
+                Console.Write('\n');
             }
 
             firstForStopWatch.Stop();
             TimeSpan firstForTimeSpan = firstForStopWatch.Elapsed;
             TimeSpan secondForTimeSpan = secondForStopWatch.Elapsed;
-            
+
+
+            string firstForElapsedTime = $"{firstForTimeSpan.Seconds:D}:{firstForTimeSpan.Milliseconds:D}:{firstForTimeSpan.Microseconds:D}:{firstForTimeSpan.Nanoseconds:D}";
+            string secondForElapsedTime = $"{secondForTimeSpan.Seconds:D}:{secondForTimeSpan.Milliseconds:D}:{secondForTimeSpan.Microseconds:D}:{secondForTimeSpan.Nanoseconds:D}";
+
+            Console.WriteLine($"First For elapsed time:{firstForElapsedTime}-Elapsed ticks:{firstForStopWatch.ElapsedTicks}-Second For elapsed time:{secondForElapsedTime}-Elapsed ticks:{secondForStopWatch.ElapsedTicks}\n");
+        }
+
+        // O(ab) - third for is a constant
+        public static void PrintUnorderedPairsWithExtraFor(int[] arrayA, int[] arrayB)
+        {
+            Stopwatch firstForStopWatch = new Stopwatch();
+            Stopwatch secondForStopWatch = new Stopwatch();
+
+            firstForStopWatch.Start();
+            for (int i = 0; i < arrayA.Length; i++)
+            {
+                secondForStopWatch.Start();
+                for (int j = 0; j < arrayA.Length; j++)
+                {
+                    for (int k = 1; k <= 50; k++)
+                    {
+                        Console.Write($"k:{k}, i:{arrayA[i]}, j:{arrayB[j]} | ");
+                    }
+
+                    secondForStopWatch.Stop();
+                    Console.Write('\n');
+                }
+            }
+
+            firstForStopWatch.Stop();
+            TimeSpan firstForTimeSpan = firstForStopWatch.Elapsed;
+            TimeSpan secondForTimeSpan = secondForStopWatch.Elapsed;
+
 
             string firstForElapsedTime = $"{firstForTimeSpan.Seconds:D}:{firstForTimeSpan.Milliseconds:D}:{firstForTimeSpan.Microseconds:D}:{firstForTimeSpan.Nanoseconds:D}";
             string secondForElapsedTime = $"{secondForTimeSpan.Seconds:D}:{secondForTimeSpan.Milliseconds:D}:{secondForTimeSpan.Microseconds:D}:{secondForTimeSpan.Nanoseconds:D}";
